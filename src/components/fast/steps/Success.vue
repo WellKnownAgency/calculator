04ab54091ef7b422a5f55941f444b81d9019fa1f
Taras <taraskim@wknown.com>
2019-05-02 17:06:43 -0400

    small fixes

--- src/components/fast/steps/Success.vue
+++ src/components/fast/steps/Success.vue
@@ -4,12 +4,11 @@
 		<template slot="body">
 			<b-row style="text-align: center; padding-bottom: 20px;">
 				<b-col>
-					<p><b>Thank You for Booking with {{$gdata.company_settings.company_name}}</b></p>
-					<p>Our manager will get in touch with you as soon as possible!</p>
-					<p>
+					<p class="wkn-text-black"><b>Thank You for Booking with {{$gdata.company_settings.company_name}}</b></p>
+					<p class="wkn-text-black">Our manager will get in touch with you as soon as possible!</p>
+					<p class="wkn-text-black">
 						You always can reach our specialists at <br><a :href="'tel:' + $gdata.company_settings.company_phone" style="color: #b22525">{{formatPhone}}</a>
 					</p>
-					<p>Do you want to manage your request and follow for the status?</p>
 					<a class="wkn-btn-action" target="_blank" :href="cabinetUrl" style="text-decoration: none">View account page</a>
 				</b-col>
 			</b-row>
